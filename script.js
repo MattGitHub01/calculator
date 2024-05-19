c130004efdd0593368eae4a8165b816dc0463c8c
MattGitHub01 <tuckermatthewcrandall@gmail.com>
2024-05-19 08:54:43 -0600

    Write JS for calculator buttons

--- script.js
+++ script.js
@@ -1,3 +1,85 @@
+const container = document.querySelector('.container');
+
+
+const calcBody = document.createElement('div');
+calcBody.classList.add('calcBody');
+container.appendChild(calcBody);
+
+const title = document.createElement('h3');
+title.textContent = 'Calculator';
+title.classList.add('title');
+calcBody.appendChild(title);
+
+
+const calcScrn = document.createElement('div');
+calcScrn.classList.add('calcScrn');
+calcBody.appendChild(calcScrn);
+
+const btnPad = document.createElement('div');
+btnPad.classList.add('btnPad');
+calcBody.appendChild(btnPad);
+
+const sevenBtn = document.createElement('button');
+sevenBtn.classList.add('sevenBtn');
+sevenBtn.textContent = '7';
+btnPad.appendChild(sevenBtn);
+
+const eightBtn = document.createElement('button');
+eightBtn.classList.add('eightBtn');
+eightBtn.textContent = '8';
+btnPad.appendChild(eightBtn);
+
+const nineBtn = document.createElement('button');
+nineBtn.classList.add('nineBtn');
+nineBtn.textContent = '9';
+btnPad.appendChild(nineBtn);
+
+const divideBtn = document.createElement('button');
+divideBtn.classList.add('divideBtn');
+divideBtn.textContent = '÷';
+btnPad.appendChild(divideBtn);
+
+const fourBtn = document.createElement('button');
+fourBtn.classList.add('fourBtn');
+fourBtn.textContent = '4';
+btnPad.appendChild(fourBtn);
+
+const fiveBtn = document.createElement('button');
+fiveBtn.classList.add('fiveBtn');
+fiveBtn.textContent = '5';
+btnPad.appendChild(fiveBtn);
+
+const sixBtn = document.createElement('button');
+sixBtn.classList.add('sixBtn');
+sixBtn.textContent = '6';
+btnPad.appendChild(sixBtn);
+
+const multiplyBtn = document.createElement('button');
+multiplyBtn.classList.add('multiplyBtn');
+multiplyBtn.textContent = 'X';
+btnPad.appendChild(multiplyBtn);
+
+const oneBtn = document.createElement('button');
+oneBtn.classList.add('oneBtn');
+oneBtn.textContent = '1';
+btnPad.appendChild(oneBtn);
+
+const twoBtn = document.createElement('button');
+twoBtn.classList.add('twoBth');
+twoBtn.textContent = '2';
+btnPad.appendChild(twoBtn);
+
+const threeBtn = document.createElement('button');
+threeBtn.classList.add('threeBtn');
+threeBtn.textContent = '3';
+btnPad.appendChild(threeBtn);
+
+const subtractBtn = document.createElement('button');
+subtractBtn.classList.add('subtractBtn');
+subtractBtn.textContent = '-';
+btnPad.appendChild(subtractBtn);
+
+
 function add(a, b) {
     return a + b
 }
@@ -14,16 +96,6 @@ function divide(a, b) {
     return a / b
 }
 
-const container = document.querySelector('.container');
+function operate(operator, a, b) {
 
-const calcBody = document.createElement('div');
-calcBody.classList.add('calcBody');
-container.appendChild(calcBody);
-
-const title = document.createElement('h3');
-title.textContent = 'Calculator';
-title.classList.add('title');
-
-const calcScrn = document.createElement('div');
-calcScrn.classList.add('calcScrn');
-calcBody.appendChild(calcScrn);
+}
